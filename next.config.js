e11a029a0b2c32b59b69e396ea6d8c1adb94e975
Lio Young <lio@werewolf.design>
2021-10-03 21:51:18 +0200

    update next config

--- next.config.js
+++ next.config.js
@@ -3,5 +3,22 @@ module.exports = {
     webpack5: true,
     images: {
         domains: ['i.scdn.co'],
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/yiff',
+                destination: 'https://github.com/himbolion/yiff',
+            },
+            {
+                source: '/elixire',
+                destination: 'https://github.com/himbolion/elixire',
+            }
+            ,
+            {
+                source: '/nsw',
+                destination: 'https://nsw2.werewolf.design',
+            }
+        ]
     }
 }
