a80af45f4552f02a66dfbf9322dd2a8aa5aef2f5
Lio Young <lio@werewolf.design>
2021-10-03 21:52:13 +0200

    projects

---
+++ utils/projects.tsx
@@ -0,0 +1,48 @@
+type Project = {
+    src: string,
+    title: string
+    link: string
+    subtitle: string
+}
+const Projects: Project[] = [
+
+    {
+        src: "/projects/sor.dog.png",
+        link: "https://sor.dog",
+        title: "YaiSor",
+        subtitle: "Linktree-esque Site for YaiSor",
+    },
+    {
+        src: "/projects/alx.rip.png",
+        link: "https://alx.rip",
+        title: "ALEX HELDT",
+        subtitle: "Web Management / Design for Alex Heldt",
+    },
+    {
+        src: "/projects/nonstop.world.png",
+        link: "https://teme.re/nsw",
+        title: "NONSTOPWORLD",
+        subtitle: "Web Development for NONSTOPWORLD Charity Event",
+    },
+    {
+        src: "/projects/bastard.ink.png",
+        link: "https://bastard.ink",
+        title: "WOLFJAW",
+        subtitle: "Linktree-esque Site for Crook",
+    },
+    {
+        src: "/projects/elixire.png",
+        link: "https://teme.re/elixire",
+        title: "Elixire Uploader",
+        subtitle: "Node Uploader for Elixi.re",
+    }
+    ,
+    {
+        src: "/projects/yiff.png",
+        link: "https://teme.re/yiff",
+        title: "yiff",
+        subtitle: "API Wrapper for Furry APIs",
+    }
+
+]
+export default Projects
