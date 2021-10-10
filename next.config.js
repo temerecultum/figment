5176b6dbe21ff7c994e49b0f80fa907d97bd9787
Lio Young <lio@werewolf.design>
2021-10-10 12:25:20 +0200

    yaml imports

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 module.exports = {
 
-    webpack5: true,
-    images: {
-        domains: ['i.scdn.co'],
+    webpack: function (config) {
+        config.module.rules.push(
+            {
+                test: /\.ya?ml$/,
+                use: 'js-yaml-loader',
+            },
+        )
+        return config
     },
+    //   images: {
+    //     domains: ['i.scdn.co'],
+    // },
     async redirects() {
         return [
             {
