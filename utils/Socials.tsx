00875a71244efc3a9dd17c0408255d336cd176b1
Joshua <git@lio.cat>
2022-05-23 13:50:46 +0200

    update socials

--- utils/Socials.tsx
+++ utils/Socials.tsx
@@ -1,4 +1,4 @@
-import { Twitter, Monster, Github, Githubsponsors } from "@icons-pack/react-simple-icons";
+import { Twitter, Monster } from "@icons-pack/react-simple-icons";
 import { ReactChild } from 'react'
 
 type Social = {
@@ -10,12 +10,8 @@ const Socials: Social[] = [
   // @ts-ignore
   { name: "Email", string: "mailto:work@werewolf.design", icon: <Monster title="Email" /> },
   { name: "Twitter", string: "https://twitter.com/temerecultum", icon: <Twitter /> },
-  { name: "Github", string: "https://github.com/himbolion", icon: <Github /> },
+  // { name: "Github", string: "https://github.com/himbolion", icon: <Github /> },
   // @ts-ignore
-  { name: "Support ", string: "https://lio.cash", icon: <Githubsponsors title="Support" /> },
-  // { name: "Github", string: "https://github.com/hokkqi", icon: <Github /> },
-  // { name: "Picarto", string: "https://picarto.tv/himbolion", icon: <PicartoDotTv /> },
-  // { name: "Telegram", string: "https://t.me/kusakidesu", icon: <Telegram /> },
-
-]
+  // { name: "Support ", string: "https://lio.cash", icon: <Githubsponsors title="Support" /> },
+  ]
 export default Socials
